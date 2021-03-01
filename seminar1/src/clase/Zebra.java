2e90d33ee2cf487f3959908cbc21fa38039c80e0
AlexandraIosif7 <62136927+AlexandraIosif7@users.noreply.github.com>
2021-03-01 15:07:04 +0200

    Add files via upload

---
+++ seminar1/src/clase/Zebra.java
@@ -0,0 +1,21 @@
+package clase;
+
+public class Zebra extends Animal{
+	
+	public int varsta;
+
+	public Zebra(String name) {
+		super(name);
+		this.varsta = 5;
+	}
+
+	public Zebra(String name, int varsta) {
+		super(name);
+		this.varsta = varsta;
+	};
+	
+	
+	
+	
+
+}
