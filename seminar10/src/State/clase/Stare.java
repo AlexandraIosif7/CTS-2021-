520394598984e2b55fc87dd007d22fe0eda2c1f2
AlexandraIosif7 <iosifalexandra7@yahoo.com>
2021-05-06 19:05:04 +0300

    State

---
+++ seminar10/src/State/clase/Stare.java
@@ -0,0 +1,6 @@
+package State.clase;
+
+//State
+public interface Stare {
+    public void modificaStare(Masa masa);
+}
---
+++ seminar10/src/State/clase/StareRezervata.java
@@ -0,0 +1,16 @@
+package State.clase;
+//State concret C
+
+public class StareRezervata implements Stare {
+    @Override
+    public void modificaStare(Masa masa) {
+        if(masa.getStare() instanceof StareLibera){
+            masa.setStare(this);
+            System.out.println("Masa cu nr "+masa.getNumar()+" a fost rezervata!");
+        }
+        else{
+            System.out.println("Masa cu nr "+ masa.getNumar() +" nu poate fi rezervata");
+        }
+
+    }
+}
